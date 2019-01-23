78cd80acfa6e76c0a8b389d60979c8eb47d0b8d5
pleshakoff <pleshakoff@gmail.com>
2019-01-23 07:37:55 +0300

    Fibonacci additional

--- src/week2/FibonacciPartialSum.java
+++ src/week2/FibonacciPartialSum.java
@@ -19,12 +19,38 @@ public class FibonacciPartialSum {
 
         return sum % 10;
     }
-    
+
+    private static long getFibonacciPartialSumFast(long from, long to) {
+
+        int from60 = (int) (from % 60);
+        int to60 = (int) (to % 60);
+
+
+        long sum = 0;
+
+        long current = 0;
+        long next  = 1;
+
+        for (long i = 0; i <= to60; ++i) {
+            if (i >= from60) {
+                sum += current;
+            }
+
+            long new_current = next;
+            next = next + current;
+            current = new_current;
+        }
+
+        return sum % 10;
+    }
+
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long from = scanner.nextLong();
         long to = scanner.nextLong();
-        System.out.println(getFibonacciPartialSumNaive(from, to));
+        System.out.println(getFibonacciPartialSumFast(from, to));
     }
 }
 
--- src/week2/FibonacciSumLastDigit.java
+++ src/week2/FibonacciSumLastDigit.java
@@ -2,9 +2,6 @@ import java.util.*;
 
 public class FibonacciSumLastDigit {
     private static long getFibonacciSumNaive(long n) {
-
-
-
         if (n <= 1)
             return n;
 
@@ -21,11 +18,34 @@ public class FibonacciSumLastDigit {
 
         return sum % 10;
     }
-    
+
+    private static long getFibonacciSumFast(long n) {
+
+        int m = (int) (n % 60);
+
+        if (m <= 1)
+            return m;
+
+
+        long previous = 0;
+        long current  = 1;
+        long sum      = 1;
+
+        for (long i = 0; i < m-1; ++i) {
+            long tmp_previous = previous;
+            previous = current;
+            current = tmp_previous + current;
+            sum += current;
+        }
+
+        return sum % 10;
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         long n = scanner.nextLong();
-        long s = getFibonacciSumNaive(n);
+        long s = getFibonacciSumFast(n);
         System.out.println(s);
     }
 }
