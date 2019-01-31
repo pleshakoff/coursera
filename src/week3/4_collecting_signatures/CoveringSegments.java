1f38179d04806ed79c593c4ce809bb98ff7ab6f3
pleshakoff <pleshakoff@gmail.com>
2019-01-31 23:55:28 +0300

    week 3 4/6

--- src/week3/4_collecting_signatures/CoveringSegments.java
+++ src/week3/4_collecting_signatures/CoveringSegments.java
@@ -1,14 +1,38 @@
 import java.util.*;
 
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+
 public class CoveringSegments {
 
     private static int[] optimalPoints(Segment[] segments) {
         //write your code here
-        int[] points = new int[2 * segments.length];
         for (int i = 0; i < segments.length; i++) {
-            points[2 * i] = segments[i].start;
-            points[2 * i + 1] = segments[i].end;
+            for (int j = i+1; j < segments.length; j++) {
+               if (segments[j].start<segments[i].start)
+               {
+                   swapArrayElements(segments,i,j);
+               }
+            }
         }
+        int[] pointsTmp = new int[segments.length];
+        int pointsCount = 0;
+        for (int i = 0; i < segments.length; i++) {
+            int leftP = segments[i].start;
+            int rightP = segments[i].end;
+            for (int j = i+1; j < segments.length; j++) {
+                if ((segments[j].start>=leftP)&&(segments[j].start<=rightP))
+                {
+                    leftP=segments[j].start;
+                    rightP=min(rightP,segments[j].end);
+                    i++;
+                }
+            }
+            pointsTmp[pointsCount]=rightP;
+            pointsCount++;
+        }
+        int[] points = new int[pointsCount];
+        System.arraycopy(pointsTmp, 0, points, 0, pointsCount);
         return points;
     }
 
@@ -20,6 +44,7 @@ public class CoveringSegments {
             this.end = end;
         }
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -36,5 +61,13 @@ public class CoveringSegments {
             System.out.print(point + " ");
         }
     }
+
+    static <T> void swapArrayElements(T[] array, int pos1, int pos2) {
+        T tmp = array[pos1];
+        array[pos1] = array[pos2];
+        array[pos2] = tmp;
+
+    }
+
 }
  
