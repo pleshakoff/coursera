de20392d5841d06dbe69f67b76c24529ae2bc873
pleshakoff <pleshakoff@gmail.com>
2019-02-05 23:01:48 +0300

    week 3 5/6

--- src/week3/5_maximum_number_of_prizes/DifferentSummands.java
+++ src/week3/5_maximum_number_of_prizes/DifferentSummands.java
@@ -1,9 +1,25 @@
 import java.util.*;
 
 public class DifferentSummands {
+
     private static List<Integer> optimalSummands(int n) {
-        List<Integer> summands = new ArrayList<Integer>();
-        //write your code here
+        List<Integer> summands = new ArrayList<>();
+        if (n<3)
+        {
+            summands.add(n);
+            return summands;
+        }
+
+
+        int maxNumber=0;
+        while (n>0) {
+            int nextNumber = maxNumber + 1;
+            if (nextNumber >= n-nextNumber)       //1 2 3
+                nextNumber = n;
+            maxNumber=nextNumber;
+            summands.add(nextNumber);
+            n-=nextNumber;
+        }
         return summands;
     }
     
--- src/week3/6_maximum_salary/LargestNumber.java
+++ src/week3/6_maximum_salary/LargestNumber.java
@@ -1,13 +1,39 @@
 import java.util.*;
 
 public class LargestNumber {
+
     private static String largestNumber(String[] a) {
         //write your code here
         String result = "";
         for (int i = 0; i < a.length; i++) {
-            result += a[i];
+            int max = 0;
+            int maxIndex=0;
+            for (int j = 0; j < a.length; j++) {
+               int current=Integer.valueOf(makeBigDigit(a[j]));
+               if (max<current)
+               {
+                   max=current;
+                   maxIndex=j;
+               }
+
+            }
+            result+=a[maxIndex];
+            a[maxIndex]="0";
+
+
         }
         return result;
+    }
+
+    static String makeBigDigit(String s)
+    {
+        String last = s.substring(s.length() - 1);
+        StringBuilder stringBuilder = new StringBuilder(s);
+        for (int i = s.length(); i < 4; i++)
+            stringBuilder.append(last);
+        return stringBuilder.toString();
+
+
     }
 
     public static void main(String[] args) {
