0f09d10dfe61cdcb6531c24c3a2f0330c89b45c3
pleshakoff <pleshakoff@gmail.com>
2019-03-17 14:39:47 +0300

    week 5 1/5

--- src/week5/ChangeDP.java
+++ src/week5/ChangeDP.java
@@ -2,8 +2,27 @@ import java.util.Scanner;
 
 public class ChangeDP {
     private static int getChange(int m) {
+        int coins[] = {1,3,4};
+
+        int valMinCoins[] = new int[m+1];
+
+        valMinCoins[0] = 0;
+        for (int money = 1; money <=m; money++) {
+            valMinCoins[money]=0;
+            for (int coin : coins)
+            {
+               if (money>=coin)
+               {
+                   int minVal = valMinCoins[money-coin]+1;
+                   if ((valMinCoins[money] == 0) || (minVal < valMinCoins[money])) {
+                       valMinCoins[money]=minVal;
+                   }
+               }
+            }
+        }
+
         //write your code here
-        return m / 4;
+        return valMinCoins[m];
     }
 
     public static void main(String[] args) {
