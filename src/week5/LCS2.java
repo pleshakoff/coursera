4761461ccd92145cd9194e015b52de17bfe44ebe
plshakoff@gmail.com <plshakoff@gmail.com>
2019-03-19 22:28:57 +0300

    week 5 4/5 in process

--- src/week5/LCS2.java
+++ src/week5/LCS2.java
@@ -3,8 +3,50 @@ import java.util.*;
 public class LCS2 {
 
     private static int lcs2(int[] a, int[] b) {
+
+        int fromSize = a.length + 1;
+        int toSize = b.length + 1;
+        int d[][] = new int[toSize][fromSize];
+        int max = 0;
+        int length = 0;
+        boolean first = true;
+
+        d[0][0] = 0;
+
+        for (int i = 1; i < toSize; i++) {
+            d[i][0] = 0;
+        }
+
+        for (int j = 1; j < fromSize; j++) {
+            d[0][j] = 0;
+        }
+
+
+        for (int j = 1; j < fromSize; j++)
+            for (int i = 1; i < toSize; i++) {
+                int prev = Math.max(Math.max(d[i - 1][j - 1], d[i][j - 1]), d[i - 1][j]);
+//                int prev = Math.max(d[i - 1][j - 1],d[i - 1][j]);
+                if (a[j - 1] == b[i - 1] && (a[j - 1]>=max || first)) {
+                    prev++;
+                    first=false;
+                    max=a[j-1];
+                    if (prev>length) {
+                        length=prev;
+                    }
+
+                }
+                d[i][j] = prev;
+            }
+
+        for (int i = 0; i < toSize; i++) {
+            for (int j = 0; j < fromSize; j++)
+                System.out.print(d[i][j] + " ");
+            System.out.println();
+        }
+
         //Write your code here
-        return Math.min(a.length, b.length);
+//        return d[b.length][a.length];
+        return length;
     }
 
     public static void main(String[] args) {
