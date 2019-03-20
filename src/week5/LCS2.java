1305a0b89035ca2bfdd7163d50b973cc6c5db7b7
pleshakoff <pleshakoff@gmail.com>
2019-03-20 16:03:40 +0300

    week 5 5/5

--- src/week5/LCS2.java
+++ src/week5/LCS2.java
@@ -4,12 +4,14 @@ public class LCS2 {
 
     private static int lcs2(int[] a, int[] b) {
 
+        if (a.length == 0 || b.length==0) {
+            return 0;
+
+        }
+
         int fromSize = a.length + 1;
         int toSize = b.length + 1;
         int d[][] = new int[toSize][fromSize];
-        int max = 0;
-        int length = 0;
-        boolean first = true;
 
         d[0][0] = 0;
 
@@ -24,29 +26,20 @@ public class LCS2 {
 
         for (int j = 1; j < fromSize; j++)
             for (int i = 1; i < toSize; i++) {
-                int prev = Math.max(Math.max(d[i - 1][j - 1], d[i][j - 1]), d[i - 1][j]);
-//                int prev = Math.max(d[i - 1][j - 1],d[i - 1][j]);
-                if (a[j - 1] == b[i - 1] && (a[j - 1]>=max || first)) {
-                    prev++;
-                    first=false;
-                    max=a[j-1];
-                    if (prev>length) {
-                        length=prev;
-                    }
-
+                int prev = Math.max((d[i][j - 1]), d[i - 1][j]);
+                if (a[j - 1] == b[i - 1]) {
+                    //если совпали то рассматриваем результат двух последовательностей без совпавших символов
+                   prev = d[i-1][j - 1]+1;
                 }
                 d[i][j] = prev;
             }
 
-        for (int i = 0; i < toSize; i++) {
-            for (int j = 0; j < fromSize; j++)
-                System.out.print(d[i][j] + " ");
-            System.out.println();
-        }
-
-        //Write your code here
-//        return d[b.length][a.length];
-        return length;
+//        for (int i = 1; i < toSize; i++) {
+//            for (int j = 1; j < fromSize; j++)
+//                System.out.print(d[i][j] + " ");
+//            System.out.println();
+//        }
+        return d[toSize-1][fromSize-1];
     }
 
     public static void main(String[] args) {
--- src/week5/LCS3.java
+++ src/week5/LCS3.java
@@ -3,8 +3,45 @@ import java.util.*;
 public class LCS3 {
 
     private static int lcs3(int[] a, int[] b, int[] c) {
-        //Write your code here
-        return Math.min(Math.min(a.length, b.length), c.length);
+        if (a.length == 0 || b.length == 0) {
+            return 0;
+
+        }
+
+        int fromSize = a.length + 1;
+        int toSize = b.length + 1;
+        int xSize = c.length + 1;
+        int d[][][] = new int[toSize][fromSize][xSize];
+        int length = 0;
+
+        d[0][0][0] = 0;
+
+        for (int i = 1; i < toSize; i++) {
+            d[i][0][0] = 0;
+        }
+
+        for (int j = 1; j < fromSize; j++) {
+            d[0][j][0] = 0;
+        }
+
+        for (int k = 1; k < xSize; k++) {
+            d[0][0][k] = 0;
+        }
+
+        for (int j = 1; j < fromSize; j++)
+            for (int i = 1; i < toSize; i++)
+                for (int k = 1; k < xSize; k++) {
+                    int prev = Math.max(Math.max((d[i][j - 1][k]), d[i - 1][j][k]),d[i][j][k-1]);
+                    if ((a[j - 1] == b[i - 1])&& (a[j - 1]==c[k - 1])&&(b[i - 1]==c[k - 1])) {
+                        //если совпали то рассматриваем результат двух последовательностей без совпавших символов
+                        prev = d[i - 1][j - 1][k-1] + 1;
+                        if (prev > length) {
+                            length = prev;
+                        }
+                    }
+                    d[i][j][k] = prev;
+                }
+        return length;
     }
 
     public static void main(String[] args) {
