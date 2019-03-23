bbcf194f664dbf1c72e9cc6d90647ef4f8db8273
plshakoff@gmail.com <plshakoff@gmail.com>
2019-03-23 18:15:55 +0300

    ds week 1 1/4

--- src/Data Structures/week1/check_brackets_in_code/check_brackets.java
+++ src/Data Structures/week1/check_brackets_in_code/check_brackets.java
@@ -9,7 +9,7 @@ class Bracket {
         this.position = position;
     }
 
-    boolean Match(char c) {
+    boolean match(char c) {
         if (this.type == '[' && c == ']')
             return true;
         if (this.type == '{' && c == '}')
@@ -24,24 +24,39 @@ class Bracket {
 }
 
 class check_brackets {
+
+
     public static void main(String[] args) throws IOException {
         InputStreamReader input_stream = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input_stream);
         String text = reader.readLine();
 
-        Stack<Bracket> opening_brackets_stack = new Stack<Bracket>();
+        Stack<Bracket> opening_brackets_stack = new Stack<>();
+
         for (int position = 0; position < text.length(); ++position) {
             char next = text.charAt(position);
 
+
             if (next == '(' || next == '[' || next == '{') {
-                // Process opening bracket, write your code here
+                opening_brackets_stack.push(new Bracket(next,position));
             }
 
             if (next == ')' || next == ']' || next == '}') {
-                // Process closing bracket, write your code here
+                if (opening_brackets_stack.empty()||!opening_brackets_stack.peek().match(next)) {
+                    System.out.println(position+1);
+                    return;
+                }
+                else {
+                    opening_brackets_stack.pop();
+                }
             }
         }
+        if (!opening_brackets_stack.empty()) {
+            System.out.println(opening_brackets_stack.pop().position+1);
 
+        }
+        else
+         System.out.println("Success");
         // Printing answer, write your code here
     }
 }
