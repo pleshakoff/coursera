2a857e387faab0a239390dcec56060bdc3126a2d
plshakoff@gmail.com <plshakoff@gmail.com>
2019-05-08 13:05:37 +0300

    week 4 1/3

--- src/Data Structures/week4/phone_book/PhoneBook.java
+++ src/Data Structures/week4/phone_book/PhoneBook.java
@@ -10,8 +10,9 @@ import java.util.StringTokenizer;
 public class PhoneBook {
 
     private FastScanner in = new FastScanner();
+
     // Keep list of all existing (i.e. not deleted yet) contacts.
-    private List<Contact> contacts = new ArrayList<>();
+    private Contact[] contacts = new Contact[1_000_0000];
 
     public static void main(String[] args) {
         new PhoneBook().processQueries();
@@ -35,31 +36,14 @@ public class PhoneBook {
 
     private void processQuery(Query query) {
         if (query.type.equals("add")) {
-            // if we already have contact with such number,
-            // we should rewrite contact's name
-            boolean wasFound = false;
-            for (Contact contact : contacts)
-                if (contact.number == query.number) {
-                    contact.name = query.name;
-                    wasFound = true;
-                    break;
-                }
-            // otherwise, just add it
-            if (!wasFound)
-                contacts.add(new Contact(query.name, query.number));
+                contacts[query.number]=new Contact(query.name,query.number) ;
         } else if (query.type.equals("del")) {
-            for (Iterator<Contact> it = contacts.iterator(); it.hasNext(); )
-                if (it.next().number == query.number) {
-                    it.remove();
-                    break;
-                }
+            contacts[query.number]=null;
         } else {
             String response = "not found";
-            for (Contact contact: contacts)
-                if (contact.number == query.number) {
-                    response = contact.name;
-                    break;
-                }
+            if (contacts[query.number] != null) {
+                response=contacts[query.number].name;
+            }
             writeResponse(response);
         }
     }
