cd93f4beb5a7649846f02f6488b1b5053fb1b50e
pleshakoff <pleshakoff@gmail.com>
2019-06-07 16:14:02 +0300

    week 3 6/7

--- kotlin/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ kotlin/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -3,33 +3,43 @@ package taxipark
 /*
  * Task #1. Find all the drivers who performed no trips.
  */
-fun TaxiPark.findFakeDrivers(): Set<Driver> =
-        TODO()
+fun TaxiPark.findFakeDrivers(): Set<Driver> = allDrivers.filter { driver -> trips.none { driver == it.driver } }.toSet()
+
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
-        TODO()
+        allPassengers.filter { passenger -> trips.count { passenger in it.passengers } >= minTrips }.toSet()
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
 fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
-        TODO()
+        allPassengers.filter { passenger -> trips.count { (passenger in it.passengers) && (it.driver == driver) } > 1 }.toSet()
 
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
 fun TaxiPark.findSmartPassengers(): Set<Passenger> =
-        TODO()
+        allPassengers.filter { passenger ->
+            val partition = trips.filter { passenger in it.passengers }.partition { it.discount ?: 0.0 > 0 }
+            return@filter partition.first.size > partition.second.size
+        }.toSet()
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
  * Return any period if many are the most frequent, return `null` if there're no trips.
  */
 fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
-    return TODO()
+
+  return  trips.groupBy {
+        val i = (it.duration / 10)*10
+        return@groupBy IntRange(i, i + 9)
+    }.maxBy<IntRange, List<Trip>, Int> { (_, list) ->
+        (list.size)
+    }?.key
+
 }
 
 /*
