51e6a273ef2c814e9ad4f94e62ac3afd3b4aba19
pleshakoff <pleshakoff@gmail.com>
2019-06-18 16:39:41 +0300

    Week 4 1/2

--- kotlin/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
+++ kotlin/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
@@ -3,5 +3,40 @@ package board
 import board.Direction.*
 
 fun createSquareBoard(width: Int): SquareBoard = TODO()
-fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
+
+class SquareBoardImpl(override val width: Int) :SquareBoard {
+
+    override fun getCellOrNull(i: Int, j: Int): Cell? {
+        return if ((i<=width)||(j<=width)) Cell(i,j) else null
+    }
+
+    override fun getCell(i: Int, j: Int): Cell {
+        require(i<=width && j<=width)
+        return Cell(i,j);
+    }
+
+    override fun getAllCells(): Collection<Cell> {
+        val cells:List<Cell>  = ArrayList()
+        var i = 0
+        var j=0
+        while ( > 0) {
+            x--
+        }
+    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+
+//fun <T> createGameBoard(width: Int): GameBoard<T> = {return null}
 
--- kotlin/Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
+++ kotlin/Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
@@ -1,5 +1,93 @@
 package rationals
 
+import java.lang.Math.abs
+import java.math.BigInteger
+import kotlin.math.abs
+
+class Rational(num:BigInteger,den:BigInteger):Comparable<Rational>{
+
+
+    override fun compareTo(other: Rational): Int {
+        return (numerator * other.denominator - other.numerator * denominator).signum()
+    }
+
+    val numerator:BigInteger
+    val denominator:BigInteger
+
+    init {
+        require(den!=BigInteger.ZERO)
+        val gcd = num.gcd(den);
+        val sign = den.signum().toBigInteger()
+        numerator = num/gcd*sign
+        denominator = den/gcd*sign
+    }
+
+    override fun toString(): String {
+        return numerator.toString() + if(denominator!= BigInteger.ONE) "/$denominator" else ""
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Rational
+
+        if (numerator != other.numerator) return false
+        if (denominator != other.denominator) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = numerator.hashCode()
+        result = 31 * result + denominator.hashCode()
+        return result
+    }
+
+
+}
+
+infix fun Int.divBy(x: Int): Rational {
+   return Rational(this.toBigInteger(),x.toBigInteger())
+}
+
+infix fun Long.divBy(x: Long): Rational {
+    return Rational(this.toBigInteger(),x.toBigInteger())
+}
+
+infix fun BigInteger.divBy(x: BigInteger): Rational {
+    return Rational(this,x)
+}
+
+fun String.toRational():Rational{
+    val split = this.split("/")
+    return Rational(split[0].toBigInteger(),if(split.size == 2)split[1].toBigInteger() else BigInteger.ONE)
+    }
+
+operator fun Rational.plus(other: Rational):Rational{
+    return Rational(this.denominator*other.numerator+other.denominator*this.numerator,this.denominator*other.denominator)
+}
+
+operator fun Rational.minus(other: Rational):Rational{
+    return Rational(other.denominator*this.numerator-this.denominator*other.numerator,this.denominator*other.denominator)
+}
+
+operator fun Rational.times(other: Rational):Rational{
+    return Rational(this.numerator*other.numerator,this.denominator*other.denominator)
+}
+
+operator fun Rational.div(other: Rational):Rational{
+    return Rational(this.numerator*other.denominator,this.denominator*other.numerator)
+}
+
+operator fun Rational.unaryMinus():Rational{
+    return Rational(this.numerator*(-1).toBigInteger(),this.denominator)
+}
+
+
+
+
+
 
 
 fun main() {
